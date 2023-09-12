a40a4681230a874842ed8ae86b2751aa6141fd27
Minh-Kop <87182623+Minh-Kop@users.noreply.github.com>
2023-09-12 09:37:25 +0700

    Change httpOnly to false

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -82,7 +82,7 @@ const createSendToken = (user, statusCode, req, res) => {
 
     res.cookie('jwt', token, {
         maxAge: config.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
-        httpOnly: true,
+        httpOnly: false,
         sameSite: 'none',
         // secure: req.secure || req.headers('x-forwarded-proto') === 'https',
         secure: true,
